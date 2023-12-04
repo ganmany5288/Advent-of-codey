237193416aad7b29a079c398b10969588d99f895
Sabrina_Ross <134316896+SabrinaRoss@users.noreply.github.com>
2023-12-04 13:44:56 -0700

    Add files via upload
    Did the second question, yes the code is bad. I am aware, I was just rushing througfh this yippie.

--- day2_rowan.py
+++ day2_rowan.py
@@ -1,21 +1,27 @@
 sum_ = 0
+sum_powers = 0
 red = 12
 green = 13
 blue = 14
 failed = False
+inside_sum = 1
+lowest = 0
+highest_red = 0
+highest_blue = 0
+highest_green = 0
 
-file_open = open("text.txt")
+file_open = open("day2_values.txt")
 
 for colluom in file_open:
     col = colluom.split(":")
     
     game_num = col[0].split()
     game_num = int(game_num[1])
-
-
+    
+    
     col = col[1]
 
-
+    first = True
 
     col = col.split(";")
     for inside_col in col:
@@ -25,22 +31,57 @@ for colluom in file_open:
             if inside_colour[1] == "red":
                 if int(inside_colour[0]) > red:
                    failed = True
-
+                if first:
+                    highest_red = int(inside_colour[0])
+                elif highest_red < int(inside_colour[0]):
+                    highest_red = int(inside_colour[0])
             if inside_colour[1] == "green":
                 if int(inside_colour[0]) > green:
                     failed = True
-                    
-
+                if first:
+                    highest_green = int(inside_colour[0])
+                elif highest_green < int(inside_colour[0]):
+                    highest_green = int(inside_colour[0])
             if inside_colour[1] == "blue":
                 if int(inside_colour[0]) > blue:
                     failed = True
-            
+                if first:
+                    highest_blue = int(inside_colour[0])
+                elif highest_blue < int(inside_colour[0]):
+                    highest_blue = int(inside_colour[0])
+
+        if first:
+            lowest = inside_sum
+            first = False
+        elif not first:
+            if lowest > inside_sum:
+                lowest = inside_sum
+        inside_sum = 1
+    
+    
+
+
+    
+    sum_powers += (highest_blue * highest_red * highest_green)
+    highest_blue = 0
+    highest_red = 0
+    highest_green = 0
+    lowest = 0
+    first = True
+
+        
+
+
+
+
+
+
     if not failed:
         sum_ += game_num
     failed = False
 
 print(sum_)
-
+print(sum_powers)
 
 
 
